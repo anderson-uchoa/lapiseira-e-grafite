e224daadb89d8e05fa466567a0581015f40e92d7
Anderson <andersonguchoa@gmail.com>
2022-10-24 16:01:21 -0300

    initial commit

---
+++ src/grafite.py
@@ -0,0 +1,22 @@
+from src.dureza import Dureza
+
+
+class Grafite:
+
+    def __init__(self, calibre: float, dureza: Dureza, tamanho: int):
+        pass
+
+    def desgastePorFolha(self):
+        return -1
+
+    def getDureza(self):
+        return None
+
+    def getCalibre(self):
+        return -1.0
+
+    def getTamanho(self):
+        return -1
+
+    def setTamanho(self, tamanho:int):
+        pass
---
+++ src/lapiseira.py
@@ -0,0 +1,25 @@
+from src.grafite import Grafite
+
+
+class Lapiseira:
+
+    def __init__(self, calibre:float):
+        pass
+
+    def inserir (self, grafite: Grafite):
+        return False
+
+    def remover(self):
+        return False
+
+    def escrever(self, folhas: int):
+        return False
+
+    def getGrafite(self):
+        return None
+
+    def getCalibre(self):
+        return -1
+
+    def getFolhasEscritas(self):
+        return -1
